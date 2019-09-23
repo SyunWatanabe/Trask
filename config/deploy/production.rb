df06a144f98ddb4cfd381e47c27147151e46d8b3
shun <syun15052@gmail.com>
2019-09-23 11:39:13 +0900

    fix about capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,14 @@
-server "3.214.201.160", user: "shun", roles: %w{app db web}
-set :ssh_options, keys: '~/.ssh/trask_key_rsa' 
+# server "3.214.201.160", user: "shun", roles: %w{app db web}
+# set :ssh_options, keys: '~/.ssh/trask_key_rsa' 
+
+server '3.214.201.160',
+   user: "shun",
+   roles: %w{web db app},
+   ssh_options: {
+       port: 22,
+       user: "shun", # overrides user setting above
+       keys: %w(~/.ssh/trask_key_rsa),
+       forward_agent: true
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+   }
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,14 +14,3 @@ if Rails.env.production?
     config.fog_directory = 'trask-app'
   end
 end
-# if Rails.env.production?
-#   CarrierWave.configure do |config|
-#     config.fog_credentials = {
-#       :provider              => 'AWS',
-#       :region                => ENV['S3_REGION'],
-#       :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-#       :aws_secret_access_key => ENV['AWS_SECRET_KEY']
-#     }
-#     config.fog_directory     =  'trask-app'
-#   end
-# end
